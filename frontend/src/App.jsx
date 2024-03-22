6e7f17381807cf78d293956d94b16375ee7e70b7
valisoa2002 <valisoaharijaona@gmail.com>
2024-03-22 09:04:14 +0300

    Création de la partie frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,42 @@
+import Calendar from "react-awesome-calendar";
+
+const events = [
+  {
+    id: 1,
+    color: "#fd3153",
+    from: "2019-05-02T18:00:00+00:00",
+    to: "2019-05-05T19:00:00+00:00",
+    title: "This is an event",
+  },
+  {
+    id: 2,
+    color: "#1ccb9e",
+    from: "2019-05-01T13:00:00+00:00",
+    to: "2019-05-05T14:00:00+00:00",
+    title: "This is another event",
+  },
+  {
+    id: 3,
+    color: "#3694DF",
+    from: "2024-05-05T13:00:00+00:00",
+    to: "2024-05-05T20:00:00+00:00",
+    title: "Retraite Mpampianatra Distrika",
+  },
+  {
+    id: 4,
+    color: "#000",
+    from: "2024-02-14T07:00:00",
+    to: "2024-02-14T20:00:00+00:00",
+    title: "Recollection VDKT Ambohimanarina",
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Calendar events={events} />;
+    </div>
+  );
+}
+
+export default App;
